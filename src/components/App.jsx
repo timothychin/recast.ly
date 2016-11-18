6694bc46dd435b42f24d710526ee2b549cb64ca5
timothychin <timm.chin@gmail.com>
2016-11-17 21:26:33 -0800

    Get lodash method debounce to work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,7 +39,7 @@ class App extends React.Component {
       this.setState({      
         videoList: videos
       });
-    }).bind(this); 
+    }); 
   }
   
   
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 var Search = (props) => (
   <div className="search-bar form-inline">
-    <input className="form-control" onChange={() => props.onVideoSearch(document.getElementsByClassName('form-control')[0].value)} type="text" />
+    <input className="form-control" onChange={_.debounce(() => props.onVideoSearch(document.getElementsByClassName('form-control')[0].value), 300)} type="text" />
     <button className="btn hidden-sm-down">
       <span className="glyphicon glyphicon-search"></span>
     </button>
